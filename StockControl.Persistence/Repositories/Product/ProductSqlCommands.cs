e44b38912c7cd5bde4b91df79cb8c2223b7e4853
Fabio Ramos Alves <fabio.r.a.x@gmail.com>
2024-08-22 01:58:09 -0300

    fix: correção part number

--- StockControl.Persistence/Repositories/Product/ProductSqlCommands.cs
+++ StockControl.Persistence/Repositories/Product/ProductSqlCommands.cs
@@ -2,34 +2,39 @@
 {
     public static class ProductSqlCommands
     {
-        public const string CreateProduct = @"
-        INSERT INTO Products (Name, PartNumber, StockQuantity, Cost)
-        VALUES (@Name, @PartNumber, @StockQuantity, @Cost);
+        public const string CreateProductSql = @"
+        INSERT INTO Products (Name,PartNumber, StockQuantity, Cost)
+        VALUES (@Name,'' , @StockQuantity, @Cost);
         SELECT LAST_INSERT_ID();";
+
+        public const string UpdateProducartNumbertSql = @"
+        UPDATE Products
+        SET PartNumber = @PartNumber
+        WHERE Id = @ProductId;";
 
         public const string InsertMovementSql = @"
         INSERT INTO StockMovements (ProductId, Quantity, MovementType, MovementDate)
         VALUES (@ProductId, @Quantity, @MovementType, NOW());";
 
-        public const string GetProductById = @"
+        public const string GetProductByIdSql = @"
         SELECT Id, Name, PartNumber, StockQuantity, Cost 
         FROM Products 
         WHERE Id = @Id;";
 
-        public const string UpdateProduct = @"
+        public const string UpdateProductSql = @"
         UPDATE Products
         SET StockQuantity = @StockQuantity
         WHERE Id = @ProductId;";
 
-        public const string DeleteProduct = @"
+        public const string DeleteProductSql = @"
         DELETE FROM Products 
         WHERE Id = @Id;";
 
-        public const string GetAllProducts = @"
+        public const string GetAllProductsSql = @"
         SELECT Id, Name, PartNumber, StockQuantity, Cost
         FROM products;";
 
-        public const string GetSalesCustsByDay = @"
+        public const string GetSalesCustsByDaySql = @"
         
         SELECT 
             p.Name AS ProductName,
